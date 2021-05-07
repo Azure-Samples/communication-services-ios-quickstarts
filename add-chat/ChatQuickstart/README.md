94b6d2e37f191371942715f3851a20763d8e2760
ravithanneeru <80485423+ravithanneeru@users.noreply.github.com>
2021-05-06 23:06:38 -0700

    Sample for add chat to your app (#11)
    * Sample for add chat to your app
    
    * Update add-chat/ChatQuickstart/README.md
    
    Co-authored-by: ddematheu2 <43075365+ddematheu2@users.noreply.github.com>

---
+++ add-chat/ChatQuickstart/README.md
@@ -0,0 +1,38 @@
+---
+page_type: sample
+languages:
+- swift
+products:
+- azure
+- azure-communication-services
+---
+
+# Quickstart: Add Chat to your App
+
+For full instructions on how to build this code sample from scratch, look at [Quickstart: Add Chat to your App](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/chat/get-started?pivots=programming-language-swift)
+
+## Prerequisites
+
+To complete this tutorial, you’ll need the following prerequisites:
+
+- An Azure account with an active subscription. [Create an account for free](https://azure.microsoft.com/free/?WT.mc_id=A261C142F). 
+- Install [Xcode](https://developer.apple.com/xcode/) and [CocoaPods](https://cocoapods.org/). You use Xcode to create an iOS application for the quickstart, and CocoaPods to install dependencies.
+- A deployed Communication Services resource. [Create a Communication Services resource](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/create-communication-resource).
+- Create two users in Azure Communication Services, and issue them a [user access token](https://docs.microsoft.com/azure/communication-services/quickstarts/access-tokens). Be sure to set the scope to chat, and note the token string as well as the userId string. In this quickstart, you create a thread with an initial participant, and then add a second participant to the thread.
+
+
+## Before running sample code
+
+1. Open an instance of PowerShell, Windows Terminal, Command Prompt or equivalent and navigate to the directory that you'd like to clone the sample to.
+2. `git clone https://github.com/Azure-Samples/Communication-Services-ios-quickstarts.git`
+3. With the `Access Token` procured in pre-requisites, add it to the **add-chat/ChatQuickstart/ViewController.swift** file. Assign your access token in line 25:
+   ```let credential =try CommunicationTokenCredential(token: "<ACCESS_TOKEN>")```
+4. With the `End point` procured in pre-requisites, add it to the **add-chat/ChatQuickstart/ViewController.swift** file. Assign end point in line 22:
+   ```let endpoint = "<ACS_RESOURCE_ENDPOINT>"```
+5. With the `Communication Services user ID` procured in pre-requisites, add it to the **add-chat/ChatQuickstart/ViewController.swift** file. Assign first user ID in line 40.
+  
+6. With the `Communication Services user ID` procured in pre-requisites, add it to the **add-chat/ChatQuickstart/ViewController.swift** file. Assign second user ID in line 131.
+
+## Run the sample
+
+- In Xcode hit the Run button to build and run the project. In the console you can view the output from the code and the logger output from the ChatClient.
---
+++ add-chat/ChatQuickstart/ViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ViewController.swift
+//  ChatQuickstart
+//
+//  Created by Akansha Gaur on 30/04/21.
+//
+
+import UIKit
+import AzureCommunication
+import AzureCommunicationChat
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+
+            let semaphore = DispatchSemaphore(value: 0)
+            DispatchQueue.global(qos: .background).async {
+                do {
+                    // <CREATE A CHAT CLIENT>
+                    let endpoint = "<ACS_RESOURCE_ENDPOINT>"
+                    let credential =
+                    try CommunicationTokenCredential(
+                        token: "<ACCESS_TOKEN>"
+                    )
+                    let options = AzureCommunicationChatClientOptions()
+
+                    let chatClient = try ChatClient(
+                        endpoint: endpoint,
+                        credential: credential,
+                        withOptions: options
+                    )
+                    
+                    // <CREATE A CHAT THREAD>
+                    let request = CreateChatThreadRequest(
+                        topic: "Quickstart",
+                        participants: [
+                            ChatParticipant(
+                                id: CommunicationUserIdentifier("<USER_ID>"),
+                                displayName: "Jack"
+                            )
+                        ]
+                    )
+
+                    var threadId: String?
+                    chatClient.create(thread: request) { result, _ in
+                        switch result {
+                        case let .success(result):
+                            threadId = result.chatThread?.id
+
+                        case .failure:
+                            fatalError("Failed to create thread.")
+                        }
+                        semaphore.signal()
+                    }
+                    semaphore.wait()
+
+                    // <LIST ALL CHAT THREADS>
+                    chatClient.listThreads { result, _ in
+                        switch result {
+                        case let .success(chatThreadItems):
+                            var iterator = chatThreadItems.syncIterator
+                                while let chatThreadItem = iterator.next() {
+                                    print("Thread id: \(chatThreadItem.id)")
+                                }
+                        case .failure:
+                            print("Failed to list threads")
+                        }
+                        semaphore.signal()
+                    }
+                    semaphore.wait()
+
+                    // <GET A CHAT THREAD CLIENT>
+                    let chatThreadClient = try chatClient.createClient(forThread: threadId!)
+
+                    // <SEND A MESSAGE>
+                    let message = SendChatMessageRequest(
+                        content: "Hello!",
+                        senderDisplayName: "Jack"
+                    )
+
+                    var messageId: String?
+
+                    chatThreadClient.send(message: message) { result, _ in
+                        switch result {
+                        case let .success(result):
+                            print("Message sent, message id: \(result.id)")
+                            messageId = result.id
+                        case .failure:
+                            print("Failed to send message")
+                        }
+                        semaphore.signal()
+                    }
+                    semaphore.wait()
+
+                    // <SEND A READ RECEIPT >
+                    if let id = messageId {
+                        chatThreadClient.sendReadReceipt(forMessage: id) { result, _ in
+                            switch result {
+                            case .success:
+                                print("Read receipt sent")
+                            case .failure:
+                                print("Failed to send read receipt")
+                            }
+                            semaphore.signal()
+                        }
+                        semaphore.wait()
+                    } else {
+                        print("Cannot send read receipt without a message id")
+                    }
+
+                    // <RECEIVE MESSAGES>
+                    chatThreadClient.listMessages { result, _ in
+                        switch result {
+                        case let .success(messages):
+                            var iterator = messages.syncIterator
+                            while let message = iterator.next() {
+                                print("Received message of type \(message.type)")
+                            }
+
+                        case .failure:
+                            print("Failed to receive messages")
+                        }
+                        semaphore.signal()
+                    }
+                    semaphore.wait()
+
+                    // <ADD A USER>
+                    let user = ChatParticipant(
+                        id: CommunicationUserIdentifier("<USER_ID>"),
+                        displayName: "Jane"
+                    )
+
+                    chatThreadClient.add(participants: [user]) { result, _ in
+                        switch result {
+                        case let .success(result):
+                            (result.invalidParticipants != nil) ? print("Added participant") : print("Error adding participant")
+                        case .failure:
+                            print("Failed to add the participant")
+                        }
+                        semaphore.signal()
+                    }
+                    semaphore.wait()
+                    
+                    // <LIST USERS>
+                    chatThreadClient.listParticipants { result, _ in
+                        switch result {
+                        case let .success(participants):
+                            var iterator = participants.syncIterator
+                            while let participant = iterator.next() {
+                                let user = participant.id as! CommunicationUserIdentifier
+                                print("User with id: \(user.identifier)")
+                            }
+                        case .failure:
+                            print("Failed to list participants")
+                        }
+                        semaphore.signal()
+                    }
+                    semaphore.wait()
+                } catch {
+                    print("Quickstart failed: \(error.localizedDescription)")
+                }
+            }
+        }
+
+}
+
