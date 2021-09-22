91f4745d84f6713f73c4f7cbfb0d09ffea76d931
Patrick Latter <palatter@microsoft.com>
2021-09-22 15:04:25 -0700

    Create project.

---
+++ ui-library-quick-start/UILibraryQuickStart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILibraryQuickStart
+//
+//  Created by Patrick Latter on 2021-09-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
