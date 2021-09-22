6b42e175a781a4285b477b3865b93b5f702c748b
Patrick Latter <palatter@microsoft.com>
2021-09-22 15:18:37 -0700

    Add code

--- ui-library-quick-start/UILibraryQuickStart/AppDelegate.swift
+++ ui-library-quick-start/UILibraryQuickStart/AppDelegate.swift
@@ -1,17 +1,12 @@
 //
-//  AppDelegate.swift
-//  UILibraryQuickStart
-//
-//  Created by Patrick Latter on 2021-09-22.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,7 +25,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
--- ui-library-quick-start/UILibraryQuickStart/SceneDelegate.swift
+++ ui-library-quick-start/UILibraryQuickStart/SceneDelegate.swift
@@ -1,16 +1,12 @@
 //
-//  SceneDelegate.swift
-//  UILibraryQuickStart
-//
-//  Created by Patrick Latter on 2021-09-22.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 //
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -46,7 +42,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
