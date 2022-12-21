8b53d7f17307309ebb414ca645439a6faae3eef8
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2022-12-20 16:51:37 -0800

    Add chat project and rename calling project

---
+++ ui-library-quick-start/Chat/Chat/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
