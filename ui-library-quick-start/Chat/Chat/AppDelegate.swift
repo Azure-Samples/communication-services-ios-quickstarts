bcff5b0010b6229692b7b7d604740c5466cf5c84
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2022-12-21 12:58:25 -0800

    Updated header

--- ui-library-quick-start/Chat/Chat/AppDelegate.swift
+++ ui-library-quick-start/Chat/Chat/AppDelegate.swift
@@ -1,8 +1,6 @@
 //
-//  AppDelegate.swift
-//  Chat
-//
-//  Created by Leah Xia on 2022-12-20.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 //
 
 import UIKit
--- ui-library-quick-start/Chat/Chat/SceneDelegate.swift
+++ ui-library-quick-start/Chat/Chat/SceneDelegate.swift
@@ -1,8 +1,6 @@
 //
-//  SceneDelegate.swift
-//  Chat
-//
-//  Created by Leah Xia on 2022-12-20.
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
 //
 
 import UIKit
